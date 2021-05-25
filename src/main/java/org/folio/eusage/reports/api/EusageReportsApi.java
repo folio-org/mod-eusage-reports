e0a8a6fab24521d69b9d57dc0d0ceb7bba6f41e4
Adam Dickmeiss <adam@indexdata.dk>
2021-05-25 18:19:19 +0200

    Streaming response

--- src/main/java/org/folio/eusage/reports/api/EusageReportsApi.java
+++ src/main/java/org/folio/eusage/reports/api/EusageReportsApi.java
@@ -2,12 +2,19 @@ package org.folio.eusage.reports.api;
 
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.openapi.RouterBuilder;
 import io.vertx.ext.web.validation.RequestParameters;
 import io.vertx.ext.web.validation.ValidationHandler;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowStream;
+import io.vertx.sqlclient.Tuple;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.okapi.common.XOkapiHeaders;
@@ -26,7 +33,7 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
     this.version = version;
   }
 
-  void failHandler(int statusCode, RoutingContext ctx, Throwable e) {
+  static void failHandler(int statusCode, RoutingContext ctx, Throwable e) {
     ctx.response().setStatusCode(statusCode);
     ctx.response().putHeader("Content-Type", "text/plain");
     String msg = null;
@@ -44,15 +51,33 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
     String tenant = params.headerParameter(XOkapiHeaders.TENANT).getString();
 
     TenantPgPool pool = TenantPgPool.pool(vertx, tenant);
-    return pool.query("SELECT * FROM " + TE_TABLE)
-        .execute()
-        .compose(rowSet -> {
-          ctx.response().setStatusCode(200);
-          ctx.response().putHeader("Content-Type", "application/json");
-          // TODO: mapping
-          ctx.end("{ \"titles\": [ ] }");
-          return Future.succeededFuture();
-        });
+    return pool.getConnection()
+        .compose(sqlConnection ->
+            sqlConnection.prepare("SELECT * FROM " + pool.getSchema() + ".te_table")
+                .compose(pq ->
+                    sqlConnection.begin().compose(tx -> {
+                      ctx.response().setChunked(true);
+                      ctx.response().putHeader("Content-Type", "application/json");
+                      ctx.response().write("{ \"titles\" : [");
+                      AtomicInteger offset = new AtomicInteger();
+                      RowStream<Row> stream = pq.createStream(50);
+                      stream.handler(row -> {
+                        if (offset.incrementAndGet() > 1) {
+                          ctx.response().write(",");
+                        }
+                        ctx.response().write(new JsonObject()
+                            .put("id", row.getUUID(0))
+                            .put("counterReportTitle", row.getString(1))
+                            .encode());
+                      });
+                      stream.endHandler(end -> {
+                        ctx.response().write("] }");
+                        ctx.response().end();
+                        tx.commit();
+                      });
+                      return Future.succeededFuture();
+                    })
+                ));
   }
 
   @Override
@@ -70,12 +95,23 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
               .failureHandler(ctx -> failHandler(400, ctx, null));
           routerBuilder
               .operation("getReportTitles")
-              .handler(ctx -> {
-                getReportTitles(vertx, ctx)
-                    .onFailure(cause -> failHandler(400, ctx, cause));
-              });
+              .handler(ctx -> getReportTitles(vertx, ctx)
+                  .onFailure(cause -> failHandler(400, ctx, cause)));
           return Future.succeededFuture(routerBuilder.createRouter());
         });
+  }
+
+  boolean lookupTenantParameter(JsonObject tenantAttributes, String key) {
+    JsonArray parameters = tenantAttributes.getJsonArray("parameters");
+    if (parameters != null) {
+      for (int i = 0; i < parameters.size(); i++) {
+        JsonObject o = parameters.getJsonObject(i);
+        if (key.equals(o.getString("key")) && "true".equals(o.getString("value"))) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   @Override
@@ -84,7 +120,7 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
       return Future.succeededFuture(); // doing nothing for disable
     }
     TenantPgPool pool = TenantPgPool.pool(vertx, tenant);
-    return pool
+    Future<Void> future = pool
         .query("CREATE TABLE IF NOT EXISTS " + TE_TABLE + " ( "
             + "id UUID PRIMARY KEY, "
             + "counterReportTitle text, "
@@ -95,8 +131,25 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
             + "kbPackageId UUID, "
             + "kbManualMatch boolean"
             + ")")
-        .execute()
-        .mapEmpty();
+        .execute().mapEmpty();
+    if (lookupTenantParameter(tenantAttributes, "loadSample")) {
+      for (int j = 0; j < 100; j++) {
+        final var titleId = j;
+        future = future.compose(res ->
+            pool.preparedQuery("INSERT INTO " + TE_TABLE
+                + "(id, counterReportTitle, counterReportId, kbTitleName,"
+                + " kbTitleId, kbPackageName, kbPackageId, kbManualMatch)"
+                + " VALUES ($1, $2, $3, $4, $5, $6, $7, $8)")
+                .execute(Tuple.tuple(List.of(UUID.randomUUID(),
+                    "counter title " + titleId, UUID.randomUUID(),
+                    "kb title name " + titleId, UUID.randomUUID(),
+                    "kb package name " + titleId, UUID.randomUUID(),
+                    false)))
+                .mapEmpty()
+        );
+      }
+    }
+    return future;
   }
 
   @Override
--- src/test/java/org/folio/eusage/reports/MainVerticleTest.java
+++ src/test/java/org/folio/eusage/reports/MainVerticleTest.java
@@ -5,13 +5,13 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.folio.okapi.common.XOkapiHeaders;
 import org.folio.tlib.postgres.TenantPgPoolContainer;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -65,12 +65,21 @@ public class MainVerticleTest {
   }
 
   @Test
-  public void testPostTenantOK(TestContext context) {
+  public void testGetTitlesNoInit(TestContext context) {
     String tenant = "testlib";
+    RestAssured.given()
+        .header(XOkapiHeaders.TENANT, tenant)
+        .get("/eusage-reports/report-titles")
+        .then().statusCode(400)
+        .header("Content-Type", is("text/plain"))
+        .body(containsString("testlib_mod_eusage_reports.te_table"));
+  }
+
+  String tenantOp(TestContext context, String tenant, JsonObject tenantAttributes, String expectedError) {
     ExtractableResponse<Response> response = RestAssured.given()
-        .header("X-Okapi-Tenant", tenant)
+        .header(XOkapiHeaders.TENANT, tenant)
         .header("Content-Type", "application/json")
-        .body("{\"module_to\" : \"mod-eusage-reports-1.0.0\"}")
+        .body(tenantAttributes.encode())
         .post("/_/tenant")
         .then().statusCode(201)
         .header("Content-Type", is("application/json"))
@@ -82,64 +91,74 @@ public class MainVerticleTest {
     context.assertEquals("/_/tenant/" + tenantJob.getString("id"), location);
 
     response = RestAssured.given()
-        .header("X-Okapi-Tenant", tenant)
+        .header(XOkapiHeaders.TENANT, tenant)
         .get(location + "?wait=10000")
         .then().statusCode(200)
         .extract();
 
     context.assertTrue(response.path("complete"));
-    context.assertEquals(null, response.path("error"));
+    context.assertEquals(expectedError, response.path("error"));
 
     RestAssured.given()
-        .header("X-Okapi-Tenant", tenant)
+        .header(XOkapiHeaders.TENANT, tenant)
         .delete(location)
         .then().statusCode(204);
+    return location;
+  }
+
+  @Test
+  public void testPostTenantOK(TestContext context) {
+    String tenant = "testlib";
+    tenantOp(context, tenant, new JsonObject()
+            .put("module_to", "mod-eusage-reports-1.0.0"), null);
+
+    ExtractableResponse<Response> response;
 
     response = RestAssured.given()
-        .header("X-Okapi-Tenant", tenant)
+        .header(XOkapiHeaders.TENANT, tenant)
         .get("/eusage-reports/report-titles")
         .then().statusCode(200)
         .header("Content-Type", is("application/json"))
         .extract();
-    context.assertEquals(new JsonObject().put("titles", new JsonArray()).encode(),
-      new JsonObject(response.body().asString()).encode());
+    JsonObject res = new JsonObject(response.body().asString());
+    context.assertEquals(0, res.getJsonArray("titles").size());
 
+    tenantOp(context, tenant, new JsonObject()
+        .put("module_from", "mod-eusage-reports-1.0.0")
+        .put("module_to", "mod-eusage-reports-1.0.1")
+        .put("parameters", new JsonArray()
+            .add(new JsonObject()
+                .put("key", "loadReference")
+                .put("value", "true")
+            )
+            .add(new JsonObject()
+                .put("key", "loadSample")
+                .put("value", "true")
+            )
+        ), null);
 
     response = RestAssured.given()
-        .header("X-Okapi-Tenant", tenant)
-        .header("Content-Type", "application/json")
-        .body("{\"module_from\" : \"mod-eusage-reports-1.0.0\"}")
-        .post("/_/tenant")
-        .then().statusCode(201)
-        .header("Content-Type", is("application/json"))
-        .body("tenant", is(tenant))
-        .extract();
-
-    location = response.header("Location");
-    tenantJob = new JsonObject(response.asString());
-    context.assertEquals("/_/tenant/" + tenantJob.getString("id"), location);
-
-    response = RestAssured.given()
-        .header("X-Okapi-Tenant", tenant)
-        .get(location + "?wait=10000")
+        .header(XOkapiHeaders.TENANT, tenant)
+        .get("/eusage-reports/report-titles")
         .then().statusCode(200)
+        .header("Content-Type", is("application/json"))
         .extract();
+    res = new JsonObject(response.body().asString());
+    context.assertEquals(100, res.getJsonArray("titles").size());
 
-    context.assertTrue(response.path("complete"));
-    context.assertEquals(null, response.path("error"));
+    // disable
+    tenantOp(context, tenant,
+        new JsonObject().put("module_from", "mod-eusage-reports-1.0.0"), null);
 
+    // purge
     RestAssured.given()
-        .header("X-Okapi-Tenant", tenant)
-        .delete(location)
-        .then().statusCode(204);
-
-    RestAssured.given()
-        .header("X-Okapi-Tenant", tenant)
+        .header(XOkapiHeaders.TENANT, tenant)
         .header("Content-Type", "application/json")
-        .body("{\"module_from\" : \"mod-eusage-reports-1.0.0\", \"purge\":true}")
+        .body(
+            new JsonObject()
+                .put("module_from", "mod-eusage-reports-1.0.0")
+                .put("purge", true).encode())
         .post("/_/tenant")
         .then().statusCode(204);
   }
-
-
 }
