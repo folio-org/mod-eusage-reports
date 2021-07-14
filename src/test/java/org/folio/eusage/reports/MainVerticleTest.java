f24e80ab9e9335e99febd3668945a94b031c7426
Adam Dickmeiss <adam@indexdata.dk>
2021-07-14 11:31:25 +0200

    Improve error handling; uuid format

--- src/test/java/org/folio/eusage/reports/MainVerticleTest.java
+++ src/test/java/org/folio/eusage/reports/MainVerticleTest.java
@@ -672,6 +672,16 @@ public class MainVerticleTest {
   }
 
   @Test
+  public void testGetTitlesBadLimit() {
+    RestAssured.given()
+        .header(XOkapiHeaders.URL, "http://localhost:" + MOCK_PORT)
+        .get("/eusage-reports/report-titles?limit=-2")
+        .then().statusCode(400)
+        .header("Content-Type", is("text/plain"))
+        .body(containsString("limit in location QUERY: value should be >= 0"));
+  }
+
+  @Test
   public void testPostTitlesFromCounterNoOkapiUrl() {
     String tenant = "testlib";
     RestAssured.given()
@@ -714,7 +724,20 @@ public class MainVerticleTest {
   }
 
   @Test
-  public void testPostTitlesBadUUID1() {
+  public void testPostTitlesBadJson() {
+    String tenant = "testlib";
+    RestAssured.given()
+        .header(XOkapiHeaders.TENANT, tenant)
+        .header("Content-Type", "application/json")
+        .body("{")
+        .post("/eusage-reports/report-titles")
+        .then().statusCode(400)
+        .header("Content-Type", is("text/plain"))
+        .body(containsString("Failed to decode"));
+  }
+
+  @Test
+  public void testPostTitlesBadId() {
     String tenant = "testlib";
     RestAssured.given()
         .header(XOkapiHeaders.TENANT, tenant)
@@ -728,11 +751,12 @@ public class MainVerticleTest {
         ).encode())
         .post("/eusage-reports/report-titles")
         .then().statusCode(400)
-        .body(is("Invalid UUID string: 1234"));
+        .header("Content-Type", is("text/plain"))
+        .body(containsString("Validation error for body application/json"));
   }
 
   @Test
-  public void testPostTitlesBadUUID2() {
+  public void testPostTitlesBadKbTitleId() {
     String tenant = "testlib";
     RestAssured.given()
         .header(XOkapiHeaders.TENANT, tenant)
@@ -746,7 +770,8 @@ public class MainVerticleTest {
         ).encode())
         .post("/eusage-reports/report-titles")
         .then().statusCode(400)
-        .body(is("Invalid UUID string: 1234"));
+        .header("Content-Type", is("text/plain"))
+        .body(containsString("Validation error for body application/json"));
   }
 
   @Test
@@ -828,7 +853,8 @@ public class MainVerticleTest {
             .encode())
         .post("/eusage-reports/report-titles/from-counter")
         .then().statusCode(400)
-        .body(is("Bad Request"));
+        .header("Content-Type", is("text/plain"))
+        .body(containsString("Validation error for body application/json"));
   }
 
   @Test
@@ -855,7 +881,8 @@ public class MainVerticleTest {
         .body(new JsonObject().put("agreementId", "1234").encode())
         .post("/eusage-reports/report-data/from-agreement")
         .then().statusCode(400)
-        .body(is("Bad Request"));
+        .header("Content-Type", is("text/plain"))
+        .body(containsString("Bad Request"));
   }
 
   @Test
@@ -1051,7 +1078,8 @@ public class MainVerticleTest {
         .body(postTitleObject.encode())
         .post("/eusage-reports/report-titles")
         .then().statusCode(400)
-        .body(is("Bad Request"));
+        .header("Content-Type", is("text/plain"))
+        .body(containsString("Validation error for body application/json"));
 
     response = RestAssured.given()
         .header(XOkapiHeaders.TENANT, tenant)
