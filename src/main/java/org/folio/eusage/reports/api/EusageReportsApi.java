70a2d556c7ad1dba8648565ae51a98a0b263dfa1
Julian Ladisch <julianladisch@users.noreply.github.com>
2021-07-15 16:41:51 +0200

    Move UseOverTimeApi.java into EusageReportsApi.java, one interface only

--- src/main/java/org/folio/eusage/reports/api/EusageReportsApi.java
+++ src/main/java/org/folio/eusage/reports/api/EusageReportsApi.java
@@ -39,6 +39,7 @@ import org.folio.okapi.common.XOkapiHeaders;
 import org.folio.tlib.RouterCreator;
 import org.folio.tlib.TenantInitHooks;
 import org.folio.tlib.postgres.TenantPgPool;
+import org.folio.tlib.util.TenantUtil;
 
 public class EusageReportsApi implements RouterCreator, TenantInitHooks {
   private static final Logger log = LogManager.getLogger(EusageReportsApi.class);
@@ -1011,6 +1012,52 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
         });
   }
 
+  /** Draft, Work in Progress (WIP) */
+  private Future<Void> getUseOverTime(Vertx vertx, RoutingContext ctx) {
+    TenantPgPool pool = TenantPgPool.pool(vertx, TenantUtil.tenant(ctx));
+    String agreementId = ctx.request().params().get("agreementId");
+    String start = ctx.request().params().get("startDate");
+    String end = ctx.request().params().get("endDate");
+
+    LocalDate startDate = LocalDate.parse(start).withDayOfMonth(1);
+    LocalDate endDate = LocalDate.parse(end).withDayOfMonth(1);
+    if (startDate.compareTo(endDate) > 0) {
+      throw new IllegalArgumentException("startDate=" + start + " is after endDate=" + end);
+    }
+
+    endDate.plusMonths(1);  // PostgreSQL range end value is exclusive
+
+    LocalDate date = startDate;
+    do {
+      LocalDate datePlusOne = date.plusMonths(1);
+      journal(pool, agreementId, date, datePlusOne);
+      date = datePlusOne;
+    }
+    while (date.compareTo(endDate) < 0);
+    return Future.succeededFuture();  // draft, Work in Progress (WIP)
+  }
+
+  /** Draft, Work in Progress (WIP) */
+  private Future<Void> journal(TenantPgPool pool, String agreementId, LocalDate date, LocalDate datePlusOne) {
+    return pool.preparedQuery(
+        "SELECT sum(COALESCE(uniqueaccesscount, 0)), min(kbtitlename), min(kbtitleid::text)"
+            + " FROM " + agreementEntriesTable(pool)
+            + " LEFT JOIN " + packageEntriesTable(pool) + " USING (kbpackageid, kbtitleid)"
+            + " LEFT JOIN " + titleEntriesTable(pool) + " USING (kbtitleid)"
+            + " LEFT JOIN " + titleDataTable(pool)
+            + "    ON " + titleEntriesTable(pool) + ".id = "
+            +     titleDataTable(pool) + ".titleentryid"
+            + " WHERE agreementid=$1 AND daterange($2, $3) @> lower(usagedaterange)"
+            + " GROUP BY kbtitleid"
+        ).execute(Tuple.of(agreementId, date, datePlusOne))
+    .map(rowSet -> {
+      rowSet.forEach(row -> {
+        row.getLong(0);
+      });
+      return null;  // draft, Work in Progress (WIP)
+    });
+  }
+
   @Override
   public Future<Router> createRouter(Vertx vertx, WebClient webClient) {
     this.webClient = webClient;
@@ -1046,6 +1093,11 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
               .handler(ctx -> postFromAgreement(vertx, ctx)
                   .onFailure(cause -> failHandler(400, ctx, cause)))
               .failureHandler(EusageReportsApi::failHandler);
+          routerBuilder
+               .operation("getUseOverTime")
+               .handler(ctx -> getUseOverTime(vertx, ctx)
+                  .onFailure(cause -> failHandler(400, ctx, cause)))
+               .failureHandler(EusageReportsApi::failHandler);
           return Future.succeededFuture(routerBuilder.createRouter());
         });
   }
