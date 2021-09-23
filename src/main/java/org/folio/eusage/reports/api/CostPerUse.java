47d9cdd8b1b10a6fa92c54f8fb0e913877c2faac
Adam Dickmeiss <adam@indexdata.dk>
2021-09-23 15:46:13 +0200

    getCostPerUse2Csv to CostPerUse class

--- src/main/java/org/folio/eusage/reports/api/CostPerUse.java
+++ src/main/java/org/folio/eusage/reports/api/CostPerUse.java
@@ -4,8 +4,10 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.concurrent.atomic.AtomicLong;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -66,7 +68,7 @@ public class CostPerUse {
     return json;
   }
 
-  static void costPerUseRows(
+  private static void costPerUseRows(
       RowSet<Row> rowSet, JsonObject json, Periods periods,
       JsonArray totalRequests, JsonArray uniqueRequests, JsonArray paidByPeriod) {
 
@@ -170,7 +172,93 @@ public class CostPerUse {
     json.put("items", items);
   }
 
-  static Number formatCost(Double n) {
+  private static Number formatCost(Double n) {
     return Double.parseDouble(costDecimalFormat.format(n));
+  }
+
+  static void getCostPerUse2Csv(JsonObject json, CSVPrinter writer) throws IOException {
+    writer.print("Agreement line");
+    writer.print("Derived Title");
+    writer.print("Print ISSN");
+    writer.print("Online ISSN");
+    writer.print("ISBN");
+    writer.print("Order type");
+    writer.print("Purchase order line");
+    writer.print("Invoice number");
+    writer.print("Fiscal start");
+    writer.print("Fiscal end");
+    writer.print("Subscription start");
+    writer.print("Subscription end");
+    writer.print("Amount encumbered");
+    writer.print("Amount paid");
+    writer.print("Total item requests");
+    writer.print("Unique item requests");
+    writer.print("Cost per request - total");
+    writer.print("Cost per request - unique");
+    writer.println();
+
+    writer.print("Totals"); // agreement line
+    writer.print(null); // publication type
+    writer.print(null); // print issn
+    writer.print(null); // online ISSN
+    writer.print(null); // ISBN
+    writer.print(null); // Order type
+    writer.print(null); // Purchase order line
+    writer.print(null); // Invoice number
+    writer.print(null);  // fiscal year start
+    writer.print(null);  // fiscal year end
+    writer.print(null);  // subscription date start
+    writer.print(null);  // subscription date end
+    writer.print(json.getDouble("amountEncumberedTotal"));
+    Double amountPaidTotal = json.getDouble("amountPaidTotal");
+    writer.print(amountPaidTotal == null ? null : formatCost(amountPaidTotal));
+    JsonArray items = json.getJsonArray("items");
+    long totalItemRequests = getTotalInLongArray(items, "totalItemRequests");
+    writer.print(totalItemRequests);
+    long uniqueItemRequests = getTotalInLongArray(items, "uniqueItemRequests");
+    writer.print(uniqueItemRequests);
+    writer.print(totalItemRequests == 0 || amountPaidTotal == null ? null
+        : formatCost(amountPaidTotal / totalItemRequests));
+    writer.print(uniqueItemRequests == 0 || amountPaidTotal == null ? null
+        : formatCost(amountPaidTotal / uniqueItemRequests));
+    writer.println();
+
+    for (int i = 0; i < items.size(); i++) {
+      JsonObject item = items.getJsonObject(i);
+      writer.print(item.getString("title"));
+      writer.print(item.getBoolean("derivedTitle") ? "Y" : "N");
+      writer.print(item.getString("printISSN"));
+      writer.print(item.getString("onlineISSN"));
+      writer.print(item.getString("ISBN"));
+      writer.print(item.getString("orderType"));
+      writer.print(orderLinesToString(item.getJsonArray("poLineIDs")));
+      writer.print(orderLinesToString(item.getJsonArray("invoiceNumbers")));
+      writer.print(item.getString("fiscalDateStart"));
+      writer.print(item.getString("fiscalDateEnd"));
+      writer.print(item.getString("subscriptionDateStart"));
+      writer.print(item.getString("subscriptionDateEnd"));
+      writer.print(item.getString("amountEncumbered"));
+      writer.print(item.getString("amountPaid"));
+      writer.print(item.getLong("totalItemRequests"));
+      writer.print(item.getLong("uniqueItemRequests"));
+      writer.print(item.getDouble("costPerTotalRequest"));
+      writer.print(item.getDouble("costPerUniqueRequest"));
+      writer.println();
+    }
+  }
+
+  private static long getTotalInLongArray(JsonArray ar, String key) {
+    long n = 0;
+    for (int i = 0; i < ar.size(); i++) {
+      n += ar.getJsonObject(i).getLong(key);
+    }
+    return n;
+  }
+
+  private static String orderLinesToString(JsonArray ar) {
+    if (ar == null) {
+      return null;
+    }
+    return String.join(" ", ar.getList());
   }
 }
--- src/main/java/org/folio/eusage/reports/api/EusageReportsApi.java
+++ src/main/java/org/folio/eusage/reports/api/EusageReportsApi.java
@@ -63,8 +63,6 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
 
   static final String LIMIT_ALL = "?limit=2147483647";
 
-  static DecimalFormat costDecimalFormat = new DecimalFormat("#.00");
-
   static String titleEntriesTable(TenantPgPool pool) {
     return pool.getSchema() + ".title_entries";
   }
@@ -1500,105 +1498,15 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
         : " AND publicationType = 'monograph'";
   }
 
-  static Number formatCost(Double n) {
-    return Double.parseDouble(costDecimalFormat.format(n));
-  }
-
-  static long getTotalInLongArray(JsonArray ar, String key) {
-    long n = 0;
-    for (int i = 0; i < ar.size(); i++) {
-      n += ar.getJsonObject(i).getLong(key);
-    }
-    return n;
-  }
-
-  static String orderLinesToString(JsonArray ar) {
-    if (ar == null) {
-      return null;
-    }
-    return String.join(" ", ar.getList());
-  }
-
   static String getCostPerUse2Csv(JsonObject json) {
     StringWriter stringWriter = new StringWriter();
     try {
       CSVPrinter writer = new CSVPrinter(stringWriter, CSV_FORMAT);
-      getCostPerUse2Csv(json, writer);
+      CostPerUse.getCostPerUse2Csv(json, writer);
     } catch (IOException e) {
       throw new UncheckedIOException(e);
     }
     return stringWriter.toString();
-  }
-
-  static void getCostPerUse2Csv(JsonObject json, CSVPrinter writer) throws IOException {
-    writer.print("Agreement line");
-    writer.print("Derived Title");
-    writer.print("Print ISSN");
-    writer.print("Online ISSN");
-    writer.print("ISBN");
-    writer.print("Order type");
-    writer.print("Purchase order line");
-    writer.print("Invoice number");
-    writer.print("Fiscal start");
-    writer.print("Fiscal end");
-    writer.print("Subscription start");
-    writer.print("Subscription end");
-    writer.print("Amount encumbered");
-    writer.print("Amount paid");
-    writer.print("Total item requests");
-    writer.print("Unique item requests");
-    writer.print("Cost per request - total");
-    writer.print("Cost per request - unique");
-    writer.println();
-
-    writer.print("Totals"); // agreement line
-    writer.print(null); // publication type
-    writer.print(null); // print issn
-    writer.print(null); // online ISSN
-    writer.print(null); // ISBN
-    writer.print(null); // Order type
-    writer.print(null); // Purchase order line
-    writer.print(null); // Invoice number
-    writer.print(null);  // fiscal year start
-    writer.print(null);  // fiscal year end
-    writer.print(null);  // subscription date start
-    writer.print(null);  // subscription date end
-    writer.print(json.getDouble("amountEncumberedTotal"));
-    Double amountPaidTotal = json.getDouble("amountPaidTotal");
-    writer.print(amountPaidTotal == null ? null : formatCost(amountPaidTotal));
-    JsonArray items = json.getJsonArray("items");
-    long totalItemRequests = getTotalInLongArray(items, "totalItemRequests");
-    writer.print(totalItemRequests);
-    long uniqueItemRequests = getTotalInLongArray(items, "uniqueItemRequests");
-    writer.print(uniqueItemRequests);
-    writer.print(totalItemRequests == 0 || amountPaidTotal == null ? null
-        : formatCost(amountPaidTotal / totalItemRequests));
-    writer.print(uniqueItemRequests == 0 || amountPaidTotal == null ? null
-        : formatCost(amountPaidTotal / uniqueItemRequests));
-    writer.println();
-
-    for (int i = 0; i < items.size(); i++) {
-      JsonObject item = items.getJsonObject(i);
-      writer.print(item.getString("title"));
-      writer.print(item.getBoolean("derivedTitle") ? "Y" : "N");
-      writer.print(item.getString("printISSN"));
-      writer.print(item.getString("onlineISSN"));
-      writer.print(item.getString("ISBN"));
-      writer.print(item.getString("orderType"));
-      writer.print(orderLinesToString(item.getJsonArray("poLineIDs")));
-      writer.print(orderLinesToString(item.getJsonArray("invoiceNumbers")));
-      writer.print(item.getString("fiscalDateStart"));
-      writer.print(item.getString("fiscalDateEnd"));
-      writer.print(item.getString("subscriptionDateStart"));
-      writer.print(item.getString("subscriptionDateEnd"));
-      writer.print(item.getString("amountEncumbered"));
-      writer.print(item.getString("amountPaid"));
-      writer.print(item.getLong("totalItemRequests"));
-      writer.print(item.getLong("uniqueItemRequests"));
-      writer.print(item.getDouble("costPerTotalRequest"));
-      writer.print(item.getDouble("costPerUniqueRequest"));
-      writer.println();
-    }
   }
 
   private static void costPerUse(StringBuilder sql, TenantPgPool pool,
@@ -1703,7 +1611,7 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
           RowIterator<Row> iterator = rowSet.iterator();
           if (!iterator.hasNext()) {
             ctx.response().setStatusCode(404);
-            ctx.response().end("No status found for id " + id.toString());
+            ctx.response().end("No status found for id " + id);
             return null;
           }
           ctx.response().setStatusCode(200);
