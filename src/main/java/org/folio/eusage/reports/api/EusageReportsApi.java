a8768e9fee99c8686d9b4f7eefcbd94c7772526f
Adam Dickmeiss <adam@indexdata.dk>
2021-09-23 13:09:55 +0200

    Only one getTitles needed

--- src/main/java/org/folio/eusage/reports/api/EusageReportsApi.java
+++ src/main/java/org/folio/eusage/reports/api/EusageReportsApi.java
@@ -1359,7 +1359,7 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
     Tuple tuple = Tuple.of(agreementId);
     periods.addStartDates(tuple);
     periods.addEnd(tuple);
-    return getTitles2(pool, isJournal, includeOA, agreementId, periods,
+    return getTitles(pool, isJournal, includeOA, agreementId, periods,
         "title, publicationDate, openAccess")
         .map(rowSet -> UseOverTime.titlesToJsonObject(rowSet, agreementId, periods));
   }
@@ -1405,7 +1405,7 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
     Periods usePeriods = new Periods(start, end, accessCountPeriod);
     int pubPeriodsInMonths = yopInterval == null || "auto".equals(yopInterval)
         ? 12 : Periods.getPeriodInMonths(yopInterval);
-    return getTitles2(pool, isJournal, includeOA, agreementId, usePeriods,
+    return getTitles(pool, isJournal, includeOA, agreementId, usePeriods,
         "title, publicationDate, openAccess")
         .map(rowSet -> ReqsByDateOfUse.titlesToJsonObject(rowSet, agreementId,
             usePeriods, pubPeriodsInMonths));
@@ -1453,34 +1453,13 @@ public class EusageReportsApi implements RouterCreator, TenantInitHooks {
     int pubPeriodsInMonths = accessCountPeriod == null || "auto".equals(accessCountPeriod)
         ? 12 : Periods.getPeriodInMonths(accessCountPeriod);
 
-    return getTitles2(pool, isJournal, includeOA, agreementId, usePeriods,
+    return getTitles(pool, isJournal, includeOA, agreementId, usePeriods,
         "title, usageDateRange, openAccess")
         .map(rowSet -> ReqsByPubYear.titlesToJsonObject(rowSet, agreementId,
             usePeriods, pubPeriodsInMonths));
   }
 
   static Future<RowSet<Row>> getTitles(TenantPgPool pool, Boolean isJournal, boolean includeOA,
-      String agreementId, Periods usePeriods, String orderBy) {
-
-    String sql = "SELECT title_entries.kbTitleId AS kbId, kbTitleName AS title,"
-        + " kbPackageId, kbPackageName, printISSN, onlineISSN, ISBN,"
-        + " publicationDate, usageDateRange, uniqueAccessCount, totalAccessCount, openAccess"
-        + " FROM " + agreementEntriesTable(pool)
-        + " LEFT JOIN " + packageEntriesTable(pool) + " USING (kbPackageId)"
-        + " JOIN " + titleEntriesTable(pool) + " ON"
-        + " title_entries.kbTitleId = agreement_entries.kbTitleId OR"
-        + " title_entries.kbTitleId = package_entries.kbTitleId"
-        + " JOIN " + titleDataTable(pool) + " ON titleEntryId = title_entries.id"
-        + " WHERE agreementId = $1"
-        + limitJournal(isJournal)
-        + "   AND daterange($2, $3) @> lower(usageDateRange)"
-        +  (includeOA ? "" : " AND NOT openAccess")
-        + " ORDER BY " + orderBy;
-    return pool.preparedQuery(sql)
-        .execute(Tuple.of(agreementId, usePeriods.startDate, usePeriods.endDate));
-  }
-
-  static Future<RowSet<Row>> getTitles2(TenantPgPool pool, Boolean isJournal, boolean includeOA,
       String agreementId, Periods usePeriods, String orderBy) {
 
     String sql = "SELECT title_entries.kbTitleId AS kbId, kbTitleName AS title,"
