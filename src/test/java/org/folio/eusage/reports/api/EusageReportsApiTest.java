c2d6cedca8df6ae284c3607dc8d3ea67c2175b98
Adam Dickmeiss <adam@indexdata.dk>
2021-09-28 15:10:43 +0200

    Refactor CSV reporting to own utility class MODEUR-97

--- src/test/java/org/folio/eusage/reports/api/EusageReportsApiTest.java
+++ src/test/java/org/folio/eusage/reports/api/EusageReportsApiTest.java
@@ -1367,7 +1367,7 @@ assertThat(json.getJsonArray("items").size(), is(4));
           System.out.println(res);
           StringReader reader = new StringReader(res);
           try {
-            CSVParser parser = new CSVParser(reader, EusageReportsApi.CSV_FORMAT);
+            CSVParser parser = new CSVParser(reader, CsvReports.CSV_FORMAT);
             List<CSVRecord> records = parser.getRecords();
             CSVRecord header = records.get(0);
             CSVRecord totals = records.get(1);
@@ -1423,7 +1423,7 @@ assertThat(json.getJsonArray("items").size(), is(4));
           String res = body.getValue();
           StringReader reader = new StringReader(res);
           try {
-            CSVParser parser = new CSVParser(reader, EusageReportsApi.CSV_FORMAT);
+            CSVParser parser = new CSVParser(reader, CsvReports.CSV_FORMAT);
             List<CSVRecord> records = parser.getRecords();
             CSVRecord header = records.get(0);
             CSVRecord totals = records.get(1);
@@ -1458,7 +1458,7 @@ assertThat(json.getJsonArray("items").size(), is(4));
           String res = body.getValue();
           StringReader reader = new StringReader(res);
           try {
-            CSVParser parser = new CSVParser(reader, EusageReportsApi.CSV_FORMAT);
+            CSVParser parser = new CSVParser(reader, CsvReports.CSV_FORMAT);
             List<CSVRecord> records = parser.getRecords();
             CSVRecord header = records.get(0);
             CSVRecord totals = records.get(1);
